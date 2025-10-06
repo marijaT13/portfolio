ee151e21ccb21dc8b68e200828247075e8d63710
marijaT13 <tashevska254@gmail.com>
2025-10-06 16:15:19 +0200

    Added animations on intro page

--- app/OutroPage.tsx
+++ app/OutroPage.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function OutroPage(){
     return(
-        <div className="flex flex-col justify-center items-center h-screen mt-5">
+        <div className="flex flex-col justify-center items-center h-screen mt-">
              <h1 className="text-9xl text-center font-sans">Let's work together!</h1>
-             <div className="flex flex-col sm:flex-row gap-4 justify-center items-center m-20">
+             <div className="flex flex-col sm:flex-row gap-4 justify-center items-center m-30">
                 <Card className="mr-50 inline-flex text-center bg-transparent border-none  shadow-xl p-2">
                 <CardContent className="p-0">
                     <Link href='mailto:marijatashevska222@gmail.com' 
@@ -15,7 +15,7 @@ export default function OutroPage(){
                 </CardContent>
                 
              </Card>
-             <Card className="ml-40 inline-flex text-center bg-transparent border-none shadow-xl p-2">
+             <Card className="ml-20 inline-flex text-center bg-transparent border-none shadow-xl p-2">
                 <CardContent className="p-0">
                     <Link href='tel:+38975978667' 
                     className="text-center underline text-red-900 hover:text-red-600">
